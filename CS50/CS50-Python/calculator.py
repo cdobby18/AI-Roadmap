fd4684111898c01dac44635448841bdc42d2a38f
Carl Joshua <carljoshuamcoloma@gmail.com>
2026-04-29 11:20:41 +0800

    units

--- CS50/CS50-Python/calculator.py
+++ CS50/CS50-Python/calculator.py
@@ -1,10 +1,16 @@
-# Unit Testing
-def main():
-    x = int(input("What is x? "))
-    print("x squared is", square(x))
-
+#Unit Testing
 def square(n):
-    return n*n
+    return n * n
+
+def main():
+    try:
+       assert square(2) == 4
+    except AssertionError:
+        print("2 Squared was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 Squared was not 9")
 
 if __name__ == "__main__":
     main()
